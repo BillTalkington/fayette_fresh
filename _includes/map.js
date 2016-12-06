171900bfd1db9a4cc7a37550046a040b9612db99
Bill Talkington <talkington.bill@gmail.com>
2016-12-05 23:52:03 -0500

    Create GroupLayer for markers

--- _includes/map.js
+++ _includes/map.js
@@ -1,26 +1,36 @@
-var mymap = L.map('mapid').setView([39.899926, -79.732910], 13);
-
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1IjoiYnRhbGsiLCJhIjoiY2l3NXVmOTR4MDBkcDJ0cDhucDZrbDY1ZyJ9.f8U38BI8VzRPf4LsFjjJcQ'}).addTo(mymap);
-
-var marker = L.marker([39.899926, -79.732910]).addTo(mymap);
-
-marker.bindPopup("<b>Hello Joe!</b><br>Let's grab a beer sometime.").openPopup();
-
+// var mymap = L.map('mapid').setView([39.899926, -79.732910], 13);
+var farms = new L.LayerGroup();
 
 {% for farm in site.data.fayettefreshfarms %}
 marker = new L.marker([{{ farm.latlon }}])
 			.bindPopup("{{ farm.name }}")
-			.addTo(mymap);
+			.addTo(farms);
 {% endfor %}
 
+var joe = new L.LayerGroup();
 
-// for (var i = 0; i < planes.length; i++) {
-// 			marker = new L.marker([planes[i][1],planes[i][2]])
-// 				.bindPopup(planes[i][0])
-// 				.addTo(map);
-// 		}
+L.marker([39.899926, -79.732910])
+			.addTo(joe)
+			.bindPopup("<b>Hello Joe!</b><br>Let's grab a beer sometime.");
+
+var streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoiYnRhbGsiLCJhIjoiY2l3NXVmOTR4MDBkcDJ0cDhucDZrbDY1ZyJ9.f8U38BI8VzRPf4LsFjjJcQ'});
+
+var map = L.map('mapid', {
+    center: [39.899926, -79.732910],
+    zoom: 10,
+    layers: [streets, farms, joe]
+});
+
+var baseLayers = {
+		"Streets": streets
+	};
+
+var overlays = {
+		"Farms": farms
+	};
+
+L.control.layers(baseLayers, overlays).addTo(map);
