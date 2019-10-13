38ca11c3396018a285b15cda0227949a89fc0b3f
Bill Talkington <btalkington@healthyhearing.com>
2019-10-13 00:24:06 -0400

    rough, working live update text box added; some formatting changes

--- _includes/site.js
+++ _includes/site.js
@@ -1,4 +1,46 @@
 
+// function myFunction() {
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById("myInput");
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName("li");
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByTagName("a")[0];
+//         txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    itemContainer = document.getElementById("item-container");
+    items = itemContainer.getElementsByClassName("modal");
+    for (i = 0; i < items.length; i++) {
+		itemName = items[i].getElementsByClassName("item-name")[0];
+		itemIntro = items[i].getElementsByClassName("modal-intro")[0];
+		itemText = itemName.textContent + ' ' + itemIntro.textContent
+        if (itemText.toUpperCase().indexOf(filter) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
+    }
+}
+
+function textMatch(item) {
+  var searchTerm = textInput.value.toLowerCase(),
+      itemText = (item.name + item.type + item.category).toLowerCase();
+  return itemText.indexOf(searchTerm) !== -1;
+}
+
+
 $("input:checkbox").click(function(event) {
 
 	farmsLayer.clearLayers();
