db3370eb9eed5b27714af42313d288d4c1a36554
Bill Talkington <btalkington@healthyhearing.com>
2019-11-03 01:26:19 -0400

    Modify liveItemUpdate to work with plain tiles

--- _includes/site.js
+++ _includes/site.js
@@ -1,19 +1,35 @@
 function liveItemUpdate() {
 	dataLayer.clearLayers(); // clear leaflet map markers
 
-    var i, input, filter, itemContainer, items, mapFilter = [];
+    var i, input, filter, itemTilesContainer, items, mapFilter = [];
     var itemName, itemIntro, itemText, txtValue, itemNumber;
 
     input = document.getElementById("liveItemSearch");
     filter = input.value.toLowerCase();
 
-    itemContainer = document.getElementById("item-container");
-    items = itemContainer.getElementsByClassName("modal");
+    // Initial modal version
+	// itemContainer = document.getElementById("item-container");
+    // items = itemContainer.getElementsByClassName("modal");
+	// for (i = 0; i < items.length; i++) {
+	// 	itemName = items[i].getElementsByClassName("item-name")[0];
+	// 	itemIntro = items[i].getElementsByClassName("modal-intro")[0];
+	// 	itemText = itemName.textContent + ' ' + itemIntro.textContent;
+	// 	itemNumber = items[i].dataset.itemnumber;
 
+	//       if (itemText.toLowerCase().indexOf(filter) > -1) {
+	//           items[i].style.display = "";
+	//           mapFilter.push(itemNumber);
+	//       } else {
+	//           items[i].style.display = "none";
+	//       }
+	//   }
+
+	itemTilesContainer = document.getElementById("item-tiles-container");
+	items = itemTilesContainer.getElementsByClassName("item");
     for (i = 0; i < items.length; i++) {
 		itemName = items[i].getElementsByClassName("item-name")[0];
 		itemIntro = items[i].getElementsByClassName("modal-intro")[0];
-		itemText = itemName.textContent + ' ' + itemIntro.textContent;
+		itemText = itemName.textContent;
 		itemNumber = items[i].dataset.itemnumber;
 
         if (itemText.toLowerCase().indexOf(filter) > -1) {
